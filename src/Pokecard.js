2ab09e7705d55b21ecadd080e62d08c4158d3b17
Victor Delgado <valexdelgado@gmail.com>
2023-03-25 18:44:27 -0700

    deleted solution files to move somewhere else

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,9 +1,13 @@
 import pokemons from './App'
+import "Pokecard.css"
+const img_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
 const Pokecard = (props) => {
+    let image = `${img_API}${props.id}.png`
   return (
     <div className="pokemonCard">
         <h2>{props.name}</h2>
+        <img src={image} alt="" />
     </div>
     )
 
